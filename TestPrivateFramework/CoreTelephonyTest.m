fbb7adf1369be089c1afa12f5362c7d79e4752b6
Jay Lee <ljql2002@163.com>
2018-08-26 17:04:45 +0800

    iOS: update

--- TestPrivateFramework/CoreTelephonyTest.m
+++ TestPrivateFramework/CoreTelephonyTest.m
@@ -11,9 +11,13 @@
 #import <CoreTelephony/CTCarrier.h>
 #import <CoreTelephony/CTTelephonyNetworkInfo.h>
 #import "Extensions.h"
+#import <dlfcn.h>
+
+extern NSString *CTSettingCopyMyPhoneNumber();
 
 @interface CoreTelephonyTest () {
     IBOutlet UITextView *txtvContent;
+    IBOutlet UIButton *btnDetect;
     CTTelephonyNetworkInfo *networkInfo;
 }
 
@@ -24,11 +28,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [btnDetect addRoundCorner:2.0f];
+    [btnDetect addBorderWithColor:UIColor.blueColor];
+    
     networkInfo = [[CTTelephonyNetworkInfo alloc] init];
     
     //当sim卡更换时弹出此窗口
     networkInfo.subscriberCellularProviderDidUpdateNotifier = ^(CTCarrier *carrier){
-        
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil
                                                         message:@"Sim card changed"
                                                        delegate:nil
@@ -48,10 +54,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)btnDetectDidClick:(id)sender {
+    [self startDetect];
+}
+
 - (void)startDetect {
     
     NSMutableAttributedString *text = [[NSMutableAttributedString alloc] init];
-    
     CTCarrier *carrier = networkInfo.subscriberCellularProvider;
     {
         NSString *value = carrier.carrierName;
@@ -88,8 +97,31 @@
         [attrText addAttribute:NSForegroundColorAttributeName value:UIColor.greenColor range:NSMakeRange(info.length-value.length-1, value.length)];
         [text appendAttributedString:attrText];
     }
+    {
+        NSString *value = self.getMyNumber;
+        NSString *info = [[NSString alloc] initWithFormat:@"phoneNumber: %@\n", value];
+        NSMutableAttributedString *attrText = [[NSMutableAttributedString alloc] initWithString:info];
+        [attrText addAttribute:NSForegroundColorAttributeName value:UIColor.greenColor range:NSMakeRange(info.length-value.length-1, value.length)];
+        [text appendAttributedString:attrText];
+    }
     
     txtvContent.attributedText = text;
+}
+
+- (NSString*)getMyNumber {
+    NSLog(@"Open CoreTelephony");
+    void *lib = dlopen("/Symbols/System/Library/Framework/CoreTelephony.framework/CoreTelephony", RTLD_LAZY);
+    NSLog(@"Get CTSettingCopyMyPhoneNumber from CoreTelephony");
+    NSString* (*pCTSettingCopyMyPhoneNumber)() = dlsym(lib, "CTSettingCopyMyPhoneNumber");
+    NSLog(@"Get CTSettingCopyMyPhoneNumber from CoreTelephony");
+    
+    if (pCTSettingCopyMyPhoneNumber == nil) {
+        NSLog(@"pCTSettingCopyMyPhoneNumber is nil");
+        return nil;
+    }
+    NSString* ownPhoneNumber = pCTSettingCopyMyPhoneNumber();
+    dlclose(lib);
+    return ownPhoneNumber;
 }
 
 @end
